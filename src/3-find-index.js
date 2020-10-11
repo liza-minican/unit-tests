8f86e660066b9a574dcd7521e48f2bc5852e3650
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2020-10-11 22:54:59 +0200

    solved tests

--- src/3-find-index.js
+++ src/3-find-index.js
@@ -1,3 +1,9 @@
 export const findIndex = (array, value) => {
-  return
-}
+  return array.indexOf(value);
+};
+// const fruits = ["apple", "banana", "cantaloupe", "blueberries", "grapefruit"];
+
+// const index = fruits.findIndex(fruit => fruit === "blueberries");
+
+// console.log(index); // 3
+// console.log(fruits[index]); // blueberries
--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,27 @@
-export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
-
-  if (type === 'string') {
-    return stringArrayOrObject === ''
+const isEmptyObj = (stringArrayOrObject) => {
+  for (var key in stringArrayOrObject) {
+    if (stringArrayOrObject.hasOwnProperty(key)) return false;
   }
+  return true;
+};
 
-  return false
-}
+export const isEmpty = (stringArrayOrObject) => {
+  const type = typeof stringArrayOrObject;
+  if (type === "string" && stringArrayOrObject === "") {
+    return true;
+  } else if (
+    Array.isArray(stringArrayOrObject) &&
+    stringArrayOrObject.length === 0
+  ) {
+    return true;
+  } else if (
+    type === "object" &&
+    type !== null &&
+    !Array.isArray(stringArrayOrObject) &&
+    isEmptyObj(stringArrayOrObject) === true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
